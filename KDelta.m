d691dfd1f357cc167b83aff786ed697d3a2483c3
Egon Geerardyn <egon.geerardyn@vub.ac.be>
2010-03-14 22:28:23 +0100

    Added KDelta function

---
+++ KDelta.m
@@ -0,0 +1,8 @@
+function [ K, Delta ] = KDelta(S)
+%KDELTA Calculates K and determinant of S matrix and matching networks
+%   Detailed explanation goes here
+Delta = det(S);
+K = (1 - norm(S(1,1))^2 - norm(S(2,2))^2 + Delta^2) / ...
+    (2*norm(S(1,2)*S(2,1)));
+end
+
