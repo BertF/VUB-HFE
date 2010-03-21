0b13469c2696affc72ad5538ab6ddd6c8162f051
Egon Geerardyn <egon.geerardyn@gmail.com>
2010-03-21 22:04:52 +0100

    MATLAB code stub for matching networks

---
+++ matcher.m
@@ -0,0 +1,10 @@
+function [ h ] = matcher(Ti, Tr, mode, demo)
+%MATCHER Matches reflection factors with integrated smith charts
+% Ti: reflection factor to start from
+% Tr: required reflection factor
+% mode: matching mode (not used)
+% demo: wait between smith chart actions for demo
+h = scDraw(0);
+
+end
+
