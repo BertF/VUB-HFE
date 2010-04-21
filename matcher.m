c5a574989ccac6d1ad07a2989561257d77ca544f
Egon Geerardyn <egon.geerardyn@gmail.com>
2010-04-21 18:05:16 +0200

    Kleine aanpassingen matcher

--- matcher.m
+++ matcher.m
@@ -35,6 +35,8 @@ function [ res, H ] = matcher(Tr)
   %% lengte TL1 bepalen
   ang = anglebetween(Gamma1,Gamma2,0,-1);
   angDeg = ang/(2*pi)*360;
+  L1 = angDeg/360*(1/2);
+  E1 = L1*360;
   
   %% eigenschappen van snijpunt bepalen
   Y2 = getZ(Gamma2);
@@ -70,21 +72,30 @@ function [ res, H ] = matcher(Tr)
   plot(partcircle(0,Gamma4,Gstub,dir),'m','LineWidth',2,'DisplayName','TL_2 (stub)');
   
   ang2Deg = ang2/(2*pi)*360;
+  L2 = ang2Deg/360*(1/2);
+  E2 = L2*360;
   
   %% export values
   res = struct('name','matching network');
   res.Gamma1 = Gamma1;
-  res.Gamma2 = Gamma2;
-  res.Gamma3 = Gamma3;
-  res.Gamma4 = Gamma4;
+  res.Gamma2 = - Gamma2; % mintekens: van getekende ZSC naar YSC transformeren
+  res.Gamma3 = - Gamma3;
+  res.Gamma4 = - Gamma4;
   res.stubterm = stubterm;
   res.Y2 = Y2;
   res.Y3 = Y3;
   res.Y4 = Y4;
   res.ang1 = ang;
   res.ang2 = ang2;
+  res.angunit = 'radian';
   res.ang1deg = angDeg;
   res.ang2deg = ang2Deg;
+  res.E1 = E1;
+  res.E2 = E2;
+  res.Eunit = 'degree';
+  res.L1 = L1;
+  res.L2 = L2;
+  res.Lunit = '\lambda';
   
 
   H = gcf;
